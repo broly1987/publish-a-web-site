b1e2054342b429fdc6aa2eeb97eec2bf3dc54876
ariel <76792188+broly1987@users.noreply.github.com>
2021-12-08 01:24:27 +0000

    add slider

--- web_site_2/main.py
+++ web_site_2/main.py
@@ -11,3 +11,7 @@ st.subheader('Introducing streamlit in Automate everything with python')
 st.write('this is our first app')
 st.write(dt)
 st.line_chart(dt)
+st.area_chart(dt)
+
+myslider = st.slider('Celsius')
+st.write(myslider,'in fahrenheit is ',myslider * 9/5 + 32)
